002b5ce32bd6c381afe200438451cb882d374793
vpcom <contact@vincentperrin.com>
2015-10-17 20:34:57 +0200

    Added the navbar directive and the HTML5 footer and header

---
+++ app/shared/navbar/navbar.directive.js
@@ -0,0 +1,29 @@
+(function() {
+    'use strict';
+
+    angular
+        .module('app')
+        .directive('navbar', navbar);
+
+    /* Directive */
+    function navbar() {
+        var directive = {
+            restrict: 'E',
+            templateUrl: 'app/shared/navbar/navbar.html',
+            controller: NavbarController,
+        };
+
+        return directive;
+
+        function NavbarController($scope, $location) {
+            // default navbar state
+            $scope.navbarCollapsed = true;
+
+            // testing the location to set the active element
+            $scope.isActive = function (currentLocation) {
+                return currentLocation === $location.path();
+            };
+
+        }
+    }
+})();
